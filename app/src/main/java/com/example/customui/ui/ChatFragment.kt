6acfb49fa2b402669e46714435b798267654447a
Kieran <57554723+ksw5@users.noreply.github.com>
2022-09-21 16:15:36 -0400

    adding showcarousel

--- app/src/main/java/com/example/customui/ui/ChatFragment.kt
+++ app/src/main/java/com/example/customui/ui/ChatFragment.kt
@@ -7,8 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.customui.BuildConfig
 import com.example.customui.databinding.FragmentChatBinding
@@ -19,7 +18,8 @@ import com.nanorep.nanoengine.PersonalInfoRequest
 import com.nanorep.nanoengine.bot.BotAccount
 import com.nanorep.nanoengine.bot.BotChat
 import com.nanorep.nanoengine.bot.BotChatListener
-import com.nanorep.nanoengine.model.NRChannel
+import com.nanorep.nanoengine.model.conversation.SessionInfoConfigKeys
+import com.nanorep.nanoengine.model.conversation.isTextOnly
 import com.nanorep.nanoengine.model.conversation.statement.InputMethod
 import com.nanorep.nanoengine.model.conversation.statement.OutgoingStatement
 import com.nanorep.nanoengine.model.conversation.statement.StatementFactory
@@ -32,6 +32,7 @@ import com.nanorep.sdkcore.utils.NRError
 
 class ChatFragment : Fragment(), ChatEventListener {
 
+
     private var _binding: FragmentChatBinding? = null
     val binding get() = _binding!!
     val apiKey = BuildConfig.ApiKey
@@ -40,15 +41,19 @@ class ChatFragment : Fragment(), ChatEventListener {
     val name = BuildConfig.Account
 
 
-    val account = BotAccount(apiKey, name, kb, server).apply { userId }
+    val account = BotAccount(apiKey, name, kb, server)
+    var textOnly = account.info.isTextOnly
+
     private lateinit var botChat: BotChat
     private lateinit var listener: BotChatListener
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         createChat()
+        textOnly = true
 
     }
 
@@ -58,22 +63,29 @@ class ChatFragment : Fragment(), ChatEventListener {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentChatBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+
+        val list = arrayOf("amazon", "google", "apple", "meta", "netflix")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, list)
+        binding.autoComplete.setAdapter(adapter)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.chatBox.setEndIconOnClickListener {
-            val userMessage = binding.editText.text
+            val userMessage = binding.autoComplete.text
             sendStatementToEngine(userMessage.toString())
             userMessage?.clear()
             it.hideKeyboard()
             binding.linearLayout.removeAllViews()
 
         }
+
+
     }
+
 
     private fun createChat() {
         botChat = BotChat(account)
@@ -86,9 +98,17 @@ class ChatFragment : Fragment(), ChatEventListener {
             override fun onResponse(response: StatementResponse) {
 
                 handleStatementResponse(response)
+                Log.d("response_type", "${response.responseType}")
+                //val formattedText = Html.fromHtml(response.text).toString()
                 binding.botResponse.text = response.text
 
+
                 val responseOptionsHandler = response.optionsHandler
+
+                if (response.responseType == "carousel") {
+                    Log.d("carousel", "carousel here")
+                    showCarousel(response)
+                }
 
                 if (responseOptionsHandler.hasQuickOptions()) {
                     showButtons(response)
@@ -104,26 +124,20 @@ class ChatFragment : Fragment(), ChatEventListener {
         }
 
         botChat.setBotChatListener(listener)
-        botChat.initConversation()
-
         botChat.setEntitiesProvider(object : EntitiesProvider {
             override fun provide(personalInfoRequest: PersonalInfoRequest, callback: PersonalInfoRequest.Callback) {
                 when (personalInfoRequest.id) { // this is the formatted hidden method that was injected into the article inside a "{{}}"
-                    "getAccountBalance" -> {
-                        callback.onInfoReady("BALANCE", null)
-                        return
-                    }
                     "NewLoan" -> {
-                        callback.onInfoReady("for an immediate loan", NRAction(personalInfoRequest.id).apply {
-                            Log.d("personalInfo", "${personalInfoRequest.id} ${personalInfoRequest.userInfo}")
-                            if (userInfo != null) {
-                                personalInfoRequest.userInfo?.let{ info -> this.userInfo.putAll(info)}
-                                //this.userInfo.putAll(personalInfoRequest.userInfo)
-                            }
-
-                        }) // the action is kind of parallel retrieved info - The customer should set it according to its needs
-                        // this action will be added to the actions list on the StatementResponse
+                callback.onInfoReady("", NRAction(personalInfoRequest.id).apply {
+                    Log.d("personalInfo", "${personalInfoRequest.id} ${personalInfoRequest.userInfo}")
+                    if (userInfo != null) {
+                        personalInfoRequest.userInfo?.let{ info -> this.userInfo.putAll(info)}
+                        //this.userInfo.putAll(personalInfoRequest.userInfo)
                     }
+
+                }) // the action is kind of parallel retrieved info - The customer should set it according to its needs
+                     // this action will be added to the actions list on the StatementResponse
+                  }
 
                 }
             }
@@ -133,8 +147,18 @@ class ChatFragment : Fragment(), ChatEventListener {
             }
 
         })
+        account.info.addConfigurations(SessionInfoConfigKeys.IsTextOnly to true)
+
+        botChat.initConversation()
+
+
 
     }
+    //אני רוצה לקחת הלוואה
+
+    //אני רוצה הלוואה
+
+    //אני רוצה לקחת הלוואה
 
 
     private fun showButtons(response: StatementResponse) {
@@ -165,6 +189,44 @@ class ChatFragment : Fragment(), ChatEventListener {
         }
     }
 
+    private fun showCarousel(response: StatementResponse) {
+        val gallery = response.multiAnswers
+        val layout = binding.linearLayout
+        val topLinearLayout = LinearLayout(requireContext())
+        val scrollView = HorizontalScrollView(requireContext())
+        gallery?.forEach { image ->
+            val galleryText = image.title
+            topLinearLayout.orientation = LinearLayout.HORIZONTAL
+            val imageView = ImageView(requireContext())
+            //topLinearLayout.orientation = HORIZONTAL
+
+            val imagesUrls = arrayListOf<String?>()
+            imagesUrls.add(image.imageUrl)
+            Log.d("image urls", imagesUrls.toString())
+            imagesUrls.forEach {
+
+                imageView.apply {
+                    tag = galleryText
+                    //val imageLayoutParams = LinearLayout.LayoutParams(2000, 2000)
+                    //setLayoutParams(imageLayoutParams)
+                }
+                GlideApp
+                    .with(requireContext())
+                    .load(it)
+                    .override(1000, 2000)
+                    .into(imageView)
+
+            }
+            topLinearLayout.addView(imageView)
+        }
+        scrollView.apply {
+            isFillViewport = true
+            addView(topLinearLayout)
+        }
+        layout.addView(scrollView)
+
+    }
+
     private fun sendStatementToEngine(statement: String) {
         Log.d("botChat", "sendStatementToEngine(), statement = $statement")
         botChat.postStatement(
@@ -179,15 +241,21 @@ class ChatFragment : Fragment(), ChatEventListener {
     }
 
     private fun handleStatementResponse(response: StatementResponse?) {
-        Log.d("bot_response", "botAnswer(), response = ${response.toString()}")
-        var botAnswer: String? = response?.text
 
+        response?.statement
+
+        //var botAnswer: String? = response?.text
     }
 
     fun View.hideKeyboard() {
         val inputManager =
             context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
